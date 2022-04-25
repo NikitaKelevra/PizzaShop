651508f6e98f416296ea4e49a09cbafb2830593a
Никита <81251222+NikitaKelevra@users.noreply.github.com>
2022-04-25 16:24:45 +0300

    Initial Commit

---
+++ PizzaShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PizzaShop
+//
+//  Created by Сперанский Никита on 25.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
